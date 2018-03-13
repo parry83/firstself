e0f922877dbff717194ad169f01b6a4136c7acd5
test name <parminder.singh83@live.com>
2018-03-13 23:55:51 +0000

    Commit self test

---
+++ src/main/java/devops/simplesproj/App.java
@@ -0,0 +1,13 @@
+package devops.simplesproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Time to test first self project" );
+    }
+}
